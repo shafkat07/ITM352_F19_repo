b364406a500f86dd696a3ad34ce7fc5e60efa505
shafkat07 <54658753+shafkat07@users.noreply.github.com>
2019-12-03 20:30:46 -1000

    Add files via upload

---
+++ Anowar_Shafkat_Assignment2/Public/product_data.js
@@ -0,0 +1,36 @@
+product_data = [
+    {
+        "type": "Utah",
+        "price": 30.00,
+        "image": "./images/Utah.jpg",
+        "description": "A Sun Rise by the Arches National Park in Moab, Utah."
+    },
+    {
+        "type": "The Washington Monument",
+        "price": 20.00,
+        "image": "./images/WS Monument.jpg",
+        "description": "A stroll on a evening in D.C."
+    },
+    {
+        "type": "Dallas Downtown",
+        "price": 60.00,
+        "image": "./images/Dallas.jpg",
+        "description": "On the Top of the Reunion Tower in Dallas, Texas."
+    },
+    {
+        "type": "Star Trail in Yosemite National Park",
+        "price": 70.00,
+        "image": "./images/Star.jpg",
+        "description": "After midnight in the deepest of Yosemite National Park, California."
+    },
+    {
+        "type": "The Setting Sun over the Mountain",
+        "price": 50.00,
+        "image": "./images/Glacier.jpg",
+        "description": "The light from the setting sun falls on a mountain at Glacier National Park, Montana."
+    }
+];
+
+if (typeof module !='undefined') {
+    module.exports=product_data;
+}
